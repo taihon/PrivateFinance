76d0ff4d1a9c35fd615dc9d8c8a41b0f4ab39de8
taihon <taihon@users.noreply.github.com>
2017-08-25 20:40:32 +0700

    added base class to inherit from

---
+++ PrivateFinance.Entities/DomainObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrivateFinance.Entities
+{
+    public abstract class DomainObject
+    {
+        public int Id { get; set; }
+    }
+}
