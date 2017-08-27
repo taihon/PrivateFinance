3753be2d81fd26c44093e6b42a4b3a8e9a79a6a6
taihon <taihon@users.noreply.github.com>
2017-08-27 21:34:44 +0700

    added dbcontext

---
+++ PrivateFinance.DB/FinanceContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using PrivateFinance.Entities;
+
+namespace PrivateFinance.DB
+{
+    public class FinanceContext : DbContext
+    {
+        public FinanceContext(DbContextOptions<FinanceContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryOperation> CategoryOperations { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Operation> Operations { get; set; }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
